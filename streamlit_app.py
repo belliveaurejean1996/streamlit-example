1b0c6e3c5be6c9a2d68dd7dc53d8dd53f6a20229
belliveaurejean1996 <53344686+belliveaurejean1996@users.noreply.github.com>
2024-01-24 15:19:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,35 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+# Let's import some packages
+import talib 
+import yfinance as yf
+import matplotlib.pyplot as plt
+from datetime import date
+from dateutil.relativedelta import relativedelta
+from tkinter import *
 import streamlit as st
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+# Stock analysis app
 """
+  
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+StockList = ['XIT.TO', 'NVDA', 'AMD', 'MSFT', 'AAPL', 'GOOGL']
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+Stock = StockList[0]
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+# Let's download stock_data and plot it
+current_date = date.today()
+Start_date = current_date - relativedelta(years=1)
+stock_data = yf.download(Stock, start=Start_date, end=current_date)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+# To create the MACD we have to code this
+macd, macdsignal, macdhist = talib.MACD(stock_data['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
+fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+ax1.set_ylabel('Price')
+ax1.plot(stock_data['Close'])
+ax2.set_ylabel('MACD')
+ax2.plot(macdsignal, color='green', label='Signal Line')
+ax2.plot(macd, color='red', label='MACD')
+ax2.bar(macdhist.index, macdhist, color='purple')
+ax1.set_title(Stock + ' : Daily Close Price and MACD')
+plt.legend()
+plt.show()
